d79cad4a80eb8dc3e95fb2bd500d79dc45891bb4
Zoltán Nagy <abesto0@gmail.com>
2015-01-28 18:32:41 +0100

    Updated documentation in add_tooltips.sh

--- add_tooltips.sh
+++ add_tooltips.sh
@@ -2,15 +2,16 @@
 cat <<EOF
 This script must be run from the yed-aws-palettes repository
 
-It assumes the .graphml files have been manually imported SVGs before the run.
+It assumes the .graphml files have been manually imported from SVGs before the run.
 The import cannot be automated due the fact that the yEd Software License Agreement explicitly
-forbids the usage of yEd in any kind of automated process
+forbids the usage of yEd in any kind of automated process.
 
 It further assumes that palette names have the form "AWS - \$CATEGORY.graphml" where "\$CATEGORY" is
 a directory in "\$1", which is the extracted AWS Simple Icons SVG archive used for the initial import.
 
 It works on the third and final assumption that the listing of the files in each category is in
-the same order as the order of the symbols in the yED palettes.
+the same order as the order of the symbols in the yED palettes (holds because yED lists the
+files the same way we do).
 EOF
 
 svgdir="$1"
